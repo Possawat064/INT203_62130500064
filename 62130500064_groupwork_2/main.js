c9a4469b1d31a744cf8a05d7c298cdb8e47e0c04
Possawat Tanam <possawat.tanam@mail.ac.th>
2021-02-03 09:40:34 +0700

    [edit]

--- 62130500064_groupwork_2/main.js
+++ 62130500064_groupwork_2/main.js
@@ -1,8 +1,9 @@
-const app = {
-    data() {
-        return {
-            msg: 'Hello, Vue3'
-        }​​
-    }​​
-}​​
-Vue.createApp(app).mount('#app')
+ const app = {
+     data() {
+         return {
+             msg: 'Hello, Vue3'
+         }
+     }
+
+ }
+ Vue.createApp(app).mount('#app')
